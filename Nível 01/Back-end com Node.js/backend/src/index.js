8b51e8aaf18f32368b21f2f9a98f00bcbc874563
raul_sigoli <raul_sigoli@hotmail.com>
2020-10-26 20:26:17 -0300

    Métodos HTTP

--- Nível 01/Back-end com Node.js/backend/src/index.js
+++ Nível 01/Back-end com Node.js/backend/src/index.js
@@ -2,10 +2,44 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
 
-app.get('/', (req, res) => {
-  return res.json({hello: 'world'});
-})
+app.get('/projects', (req, res) => {
+  return res.json([
+    "Projeto 1",
+    "Projeto 2"
+  ]);
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    "Projeto 1",
+    "Projeto 2",
+    "Projeto 3"
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    "Projeto 4",
+    "Projeto 2",
+    "Projeto 3"
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    "Projeto 2",
+    "Projeto 3"
+  ]);
+});
 
 app.listen(3333, () => {
   console.log(`Back-end started!🚀`)
