8b43b4820f1c8a60eeb221be4c53f80381b1db08
raul_sigoli <raul_sigoli@hotmail.com>
2020-11-01 16:43:20 -0300

    Estado e Imutabilidade

--- Nível 01/Front-end com ReactJS/front-end/src/App.js
+++ Nível 01/Front-end com ReactJS/front-end/src/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 /**
  * Componente
  * Propriedade
- * Estado
+ * Estado & Imutabilidade
  */
 
-function App() {
+export default function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  //useState retorna um arry com 2 posições
+  //
+  // 1. Variavel com seu valor inicial
+  // 2. Função para atualizar esse valor
+
+  function handleAddProject() {
+    // projects.push(`novo project ${Date.now()}`);
+
+    setProjects([...projects, `novo project ${Date.now()}`]);
+
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title="Homepage" >
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects" >
-        <ul>
-          <li>Homepage</li>
-          <li>projects</li>
-          <li>login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   );
 }
-
-export default App;
