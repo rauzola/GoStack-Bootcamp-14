8c4c2526360b35504f2b25671c0a3e261051fe1c
raul_sigoli <raul_sigoli@hotmail.com>
2020-11-01 15:28:13 -0300

    Propriedades

--- Nível 01/Front-end com ReactJS/front-end/src/App.js
+++ Nível 01/Front-end com ReactJS/front-end/src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 
 import Header from './components/Header';
 
+/**
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App() {
   return (
     <>
-      <Header />
-      <Header />
-      <Header />
+      <Header title="Homepage" >
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects" >
+        <ul>
+          <li>Homepage</li>
+          <li>projects</li>
+          <li>login</li>
+        </ul>
+      </Header>
     </>
   );
 }
