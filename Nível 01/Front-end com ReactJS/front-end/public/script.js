dd2718bebf31079fa8cd910cc383a7860290b387
raul_sigoli <raul_sigoli@hotmail.com>
2020-11-01 13:28:18 -0300

    Configurando Webpack

---
+++ Nível 01/Front-end com ReactJS/front-end/public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (tranpilar) código do React para um códigoque o browser entenda
+ * Webpack: Pra cada tipo de arquivo (js, .css, .png) eu vou converter o códigode uma maneira diferente
+ * 
+ * Loaders: babel-loader, ccs-loader, image-loader
+ */
