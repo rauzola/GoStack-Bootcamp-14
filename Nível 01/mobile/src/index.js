956effb29f903845eb10183740f431533eb86c11
rauzola <raul_sigoli@hotmail.com>
2020-11-05 22:48:23 -0300

    Criando novos projetos

--- Nível 01/mobile/src/index.js
+++ Nível 01/mobile/src/index.js
@@ -1,41 +1,51 @@
-import React, { useState, useEffect} from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
-// Não possuem valor semantico (significado)
-// Não possuem estilização propria
-// Todos componentes possuem pro padrão 'display: flex'
-
-// View: div, footer, header, main, aside, section
-// Text: p, span, strong, h1, h2, h3
-
 export default function App() {
+  const [projects, setProjects] = useState([]);
 
-    const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
-    useEffect(() => {
-        api.get('projects').then(res => {
-            setProjects(res.data);
-        })
-    }, []);
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Diego Fernandes'
+    });
 
-    return (
-        <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-            
-            <SafeAreaView style={styles.container}>
-                <FlatList 
-                    data={projects}
-                    keyExtractor={project => project.id}
-                    renderItem={({ item: project }) => (
-                        <Text style={styles.project}>{project.title}</Text>
+    const project = response.data;
 
-                    )}
-                />
-            </SafeAreaView>
-        </>
-    );
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -47,5 +57,19 @@ const styles = StyleSheet.create({
     project: {
         color: '#fff',
         fontSize: 20,
+    },
+
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 });
