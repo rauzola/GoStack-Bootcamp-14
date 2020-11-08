d583bd728bc3a5e7f740f1c7832fd940c5c9f89f
rauzola <raul_sigoli@hotmail.com>
2020-11-08 17:29:37 -0300

    Tipando objetos e vetores

--- Nível 01/Typescript/src/routes.ts
+++ Nível 01/Typescript/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
+
+//string, number, boolean, object, Array,
+// interface
 
 export function helloWorld(req: Request, res: Response) {
+    const user = createUser({
+        email: 'raul_sigoli@hotmail.com',
+        password: '123456',
+        techs: ['Node.JS', 'ReactJS', 'React Native'],
+    });
+
+    console.log(user.)
+
     return res.json({ message: 'hello world' });
 }
