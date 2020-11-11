4103e60d9d62e0500c87b49c51ac3d1e36df169c
rauzola <raul_sigoli@hotmail.com>
2020-11-11 19:32:00 -0300

    Padrões de Projeto com Eslint, Prettier e Editor Config

--- Nível 02/Primeiro projeto com Node.JS/src/server.ts
+++ Nível 02/Primeiro projeto com Node.JS/src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
+
+import routes from "./routes";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World!' });
-})
+app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
 app.listen(3333, () => {
-    console.log(`^_^ Back-end started ^_^`)
-  });
+  console.log("^_^ Back-end started ^_^");
+});
