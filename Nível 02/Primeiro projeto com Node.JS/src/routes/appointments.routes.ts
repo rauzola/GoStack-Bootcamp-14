54285bc8d6971c609ed0e4740eef9cb6c40e6ef7
rauzola <raul_sigoli@hotmail.com>
2020-11-11 20:49:14 -0300

    Validando a data

--- Nível 02/Primeiro projeto com Node.JS/src/routes/appointments.routes.ts
+++ Nível 02/Primeiro projeto com Node.JS/src/routes/appointments.routes.ts
@@ -1,22 +1,38 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
 
-appointmentsRouter.post('/', (req, res) => {
-  const { provider, date } = req.body;
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'Essa Hora já está agendado' });
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
 
-  return res.json(appointment);
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
