5e3f3a387b5579084b505947fb158efcdd9cc182
rauzola <raul_sigoli@hotmail.com>
2020-11-11 19:46:50 -0300

    Debugando NodeJS

--- Nível 02/Primeiro projeto com Node.JS/src/server.ts
+++ Nível 02/Primeiro projeto com Node.JS/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
-
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Hello World!" }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log("^_^ Back-end started ^_^");
+  console.log('^_^ Back-end started ^_^');
 });
