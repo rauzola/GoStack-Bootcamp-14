f86a577b10b84678fab190c787100d082fd6ea39
rauzola <raul_sigoli@hotmail.com>
2020-11-19 23:04:30 -0300

    Gerando token JWT

--- Nível 02/Iniciando back-end do app/src/services/AuthenticateUserService.ts
+++ Nível 02/Iniciando back-end do app/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -33,8 +35,14 @@ class AuthenticateUserService {
 
     // usuario autenticado
 
+    const token = sign({}, 'e1fb72c969075767273c60628ce1abf0', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
