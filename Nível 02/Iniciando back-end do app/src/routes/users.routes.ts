2d93ca5f6f2a8138285f017cedaa72f81f691244
rauzola <raul_sigoli@hotmail.com>
2020-11-21 17:38:13 -0300

    Atualizando avatar

--- Nível 02/Iniciando back-end do app/src/routes/users.routes.ts
+++ Nível 02/Iniciando back-end do app/src/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '../config/upload';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -34,9 +35,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
 
-    return response.json({ ok: true });
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
