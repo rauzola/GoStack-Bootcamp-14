751ff8226f465ca28ceab089a4d45fd3c4158724
rauzola <raul_sigoli@hotmail.com>
2020-12-17 15:25:24 -0300

    Conectando a API

--- Nível 03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nível 03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,73 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositorio" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositorio"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/57502280?s=460&u=ed105966d0a84b65124f66b51889830f456d96e2&v=4"
-            alt="Raul Sigoli"
-          />
-          <div>
-            <strong>rauzola/GoStack-Bootcamp-14</strong>
-            <p>Código Desenvolvido ao longo do GoStack Bootcamp da Turma 14</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/57502280?s=460&u=ed105966d0a84b65124f66b51889830f456d96e2&v=4"
-            alt="Raul Sigoli"
-          />
-          <div>
-            <strong>rauzola/GoStack-Bootcamp-14</strong>
-            <p>Código Desenvolvido ao longo do GoStack Bootcamp da Turma 14</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/57502280?s=460&u=ed105966d0a84b65124f66b51889830f456d96e2&v=4"
-            alt="Raul Sigoli"
-          />
-          <div>
-            <strong>rauzola/GoStack-Bootcamp-14</strong>
-            <p>Código Desenvolvido ao longo do GoStack Bootcamp da Turma 14</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/57502280?s=460&u=ed105966d0a84b65124f66b51889830f456d96e2&v=4"
-            alt="Raul Sigoli"
-          />
-          <div>
-            <strong>rauzola/GoStack-Bootcamp-14</strong>
-            <p>Código Desenvolvido ao longo do GoStack Bootcamp da Turma 14</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
