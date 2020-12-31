e0b35a26a4f7490a43278a1d89f30f21dfec1045
rauzola <raul_sigoli@hotmail.com>
2020-12-31 15:12:59 -0300

    Validação de login

--- Nível 03/gobaber-web/src/App.tsx
+++ Nível 03/gobaber-web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// import SingnIn from './pages/SignIn';
-import SingnUp from './pages/SignUp';
+import SingnIn from './pages/SignIn';
+// import SingnUp from './pages/SignUp';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SingnUp />
+    <SingnIn />
     <GlobalStyles />
   </>
 );
