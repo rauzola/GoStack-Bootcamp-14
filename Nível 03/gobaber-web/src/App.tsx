f3ecd8482698ea9c172fb6181dfdbf0e064d06bd
rauzola <raul_sigoli@hotmail.com>
2021-01-16 23:56:48 -0300

    Configurando Rotas

--- Nível 03/gobaber-web/src/App.tsx
+++ Nível 03/gobaber-web/src/App.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import SingnIn from './pages/SignIn';
-// import SingnUp from './pages/SignUp';
 import GlobalStyles from './styles/global';
 
 import AppProvider from './hooks';
 
+import Routes from './routes';
+
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SingnIn />
+      <Routes />
     </AppProvider>
 
     <GlobalStyles />
-  </>
+  </Router>
 );
 
 export default App;
--- Nível 03/gobaber-web/src/pages/SignUp/index.tsx
+++ Nível 03/gobaber-web/src/pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -11,12 +12,12 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async data => {
+  const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
 
@@ -32,38 +33,46 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      const errors = getValidationErrors(err);
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
 
-      formRef.current?.setErrors(errors);
+        formRef.current?.setErrors(errors);
+      }
     }
   }, []);
 
   return (
     <Container>
       <Background />
-
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
-          <Button type="submit">Cadastrar</Button>
-        </Form>
-        <a href="signup">
-          <FiArrowLeft />
-          Voltar para logon
-        </a>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit">Cadastrar</Button>
+          </Form>
+
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para logon
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
 };
+
 export default SignUp;
