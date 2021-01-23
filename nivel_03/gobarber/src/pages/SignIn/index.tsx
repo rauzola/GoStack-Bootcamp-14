cd7a33bd128871e42c3708e538d209247fe9af80
rauzola <raul_sigoli@hotmail.com>
2021-01-22 22:01:41 -0300

    Tela de cadastro

--- nivel_03/gobarber/src/pages/SignIn/index.tsx
+++ nivel_03/gobarber/src/pages/SignIn/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +24,8 @@ import {
 } from './styles';
 
 const SingIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -54,7 +63,7 @@ const SingIn: React.FC = () => {
           </Container >
         </ScrollView>
 
-        <CreateAccountButton onPress={() => { }}>
+        <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
           <Icon name="log-in" size={20} color="#ff9000" />
           <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
         </CreateAccountButton>
--- nivel_03/gobarber/src/pages/SignUp/index.tsx
+++ nivel_03/gobarber/src/pages/SignUp/index.tsx
@@ -1,11 +1,68 @@
 import React from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
 
-const SignIn: React.FC = () => {
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  BackToSignIn,
+  BackToSignInText,
+} from './styles';
+
+const SignUp: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container >
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('Deu');
+              }}
+            >
+              Entrar
+      </Button>
+
+
+          </Container >
+        </ScrollView>
+
+        <BackToSignIn onPress={() => navigation.navigate('SignIn')/* navigation.goBack() */}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      </KeyboardAvoidingView>
+    </>
   )
 }
 
-export default SignIn;
+export default SignUp;
